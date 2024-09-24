b51908e1a58063168648ae44b80594c78d003d97
Badamaa312 <badamaa312@gmail.com>
2024-09-24 11:16:30 +0800

    Last one

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,19 +1,22 @@
 import { useState, useEffect } from "react";
 import { BackIcon } from "../svg/BackIcon";
 import { ForwardIcon } from "../svg/ForwardIcon";
-import { generatMonth } from "../util/generateMonth";
-import Link from "next/link";
-import { Main } from "next/document";
 
-export const Carousel = (article) => {
+import Link from "next/link";
+
+export const Carousel = () => {
   const [articles, setArticles] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const publishedDate = new Date(article.published_at);
+  // const publishedDate = new Date(article.published_at);
 
-  const fetchCarouselData = () => {
-    fetch(`https://dev.to/api/articles?top=1`)
-      .then((response) => response.json())
-      .then((data) => setArticles(data));
+  const fetchCarouselData = async () => {
+    try {
+      const response = await fetch(`https://dev.to/api/articles?`);
+      const data = await response.json();
+      setArticles(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -35,38 +38,44 @@ export const Carousel = (article) => {
   return (
     <main>
       <div className="w-full flex justify-between items-center flex-col">
-        {/* <Link href={`blogs/${articles[currentIndex]?.id}`}> */}
-        <div className="container flex justify-between items-end">
-          <div className=" flex h-[780px] w-full m-auto  py-16 relative">
-            <div
-              style={{
-                backgroundImage: `url(${articles[currentIndex]?.cover_image})`,
-              }}
-              className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-            >
-              <div className="w-[598px] h-[252px] absolute bottom-20 rounded-xl bg-white ml-[11px] p-10">
-                <div>
-                  <span className="  text-white rounded-md bg-[#4B6BFB] pl-4 pr-4">
-                    {articles[currentIndex]?.tag_list}
-                  </span>
-                </div>
-                <div className="overflow-hidden h-[100px]">
-                  <p className="text-xl font-semibold text-ellipsis mt-[16px] mb-[24px] ">
-                    {articles[currentIndex]?.description}
-                  </p>
-                </div>
-                <div>
-                  <p className=" text-[#97989F]">
-                    {publishedDate?.getFullYear()}-
-                    {generatMonth(publishedDate?.getMonth())}-
-                    {publishedDate?.getDay() + 1}
-                  </p>
+        <div>
+          <Link href={`blogs/${articles[currentIndex]?.id}`}>
+            <div className="container flex justify-between items-end">
+              <div className=" flex h-[780px] w-full m-auto  py-16 ">
+                <div
+                  style={{
+                    backgroundColor: "red",
+                    width: "100vw",
+                    height: "500px",
+                    // backgroundImage: `url(${articles[currentIndex]?.cover_image})`,
+                  }}
+                  className="w-full h-full rounded-2xl bg-center bg-cover duration-500 relative"
+                >
+                  <div className="w-[598px] h-[252px] absolute bottom-20 rounded-xl bg-white ml-[11px] p-10">
+                    <div>
+                      <span className="  text-white rounded-md bg-[#4B6BFB] pl-4 pr-4">
+                        {articles[currentIndex]?.tag_list}
+                      </span>
+                    </div>
+                    <div className="overflow-hidden h-[100px]">
+                      <p className="text-xl font-semibold text-ellipsis mt-[16px] mb-[24px] ">
+                        {articles[currentIndex]?.description}
+                      </p>
+                    </div>
+                    {/* <div>
+             <p className=" text-[#97989F]">
+               {publishedDate?.getFullYear()}-
+               {generatMonth(publishedDate?.getMonth())}-
+               {publishedDate?.getDay() + 1}
+             </p>
+           </div> */}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
-        {/* </Link> */}
+
         <div className="container flex justify-end gap-[9px] ">
           <button onClick={changeBackImage}>
             <BackIcon />
